cb69ca8d0d02ebfff78314b78e0eb1ae5284172b
Robbe Thys <740392+wthys@users.noreply.github.com>
2025-12-12 12:59:55 +0100

    day 11 part 2 experiment

--- src/solutions/day11/solution.go
+++ src/solutions/day11/solution.go
@@ -32,7 +32,65 @@ func (s solution) Part1(input []string, opts solver.Options) (string, error) {
 }
 
 func (s solution) Part2(input []string, opts solver.Options) (string, error) {
-	return solver.NotImplemented()
+	connections, err := readInput(input)
+	if err != nil {
+		return solver.Error(err)
+	}
+
+	if len(connections) < 50 {
+		extraExample := []string{
+			"svr: aaa bbb",
+			"aaa: fft",
+			"fft: ccc",
+			"bbb: tty",
+			"tty: ccc",
+			"ccc: ddd eee",
+			"ddd: hub",
+			"hub: fff",
+			"eee: dac",
+			"dac: fff",
+			"fff: ggg hhh",
+			"ggg: out",
+			"hhh: out",
+		}
+		connections, _ = readInput(extraExample)
+	}
+
+
+
+
+
+
+	bfs := PF.ConstructBreadthFirst("svr", connections.NeejberFunc())
+	dacbfs := PF.ConstructBreadthFirst("dac", connections.NeejberFunc())
+	fftbfs := PF.ConstructBreadthFirst("fft", connections.NeejberFunc())
+
+
+	svrfft := 0
+	bfs.AllPathsFunc("fft", func(_ []string) {
+		svrfft += 1
+		if svrfft % 10 == 0 {
+			opts.Debugf("svr ... fft : %v found\n", svrfft)
+		}
+	})
+
+	fftdac := 0
+	fftbfs.AllPathsFunc("dac", func(_ []string) {
+		fftdac += 1
+		if fftdac % 10 == 0 {
+			opts.Debugf("fft ... dac : %v found\n", fftdac)
+		}
+	})
+
+	dacout := 0
+	dacbfs.AllPathsFunc("out", func(_ []string) {
+		dacout += 1
+		if dacout % 10 == 0 {
+			opts.Debugf("dac ... out : %v found\n", dacout)
+		}
+	})
+
+	return solver.Solved(svrfft * fftdac * dacout)
 }
 
 type (
